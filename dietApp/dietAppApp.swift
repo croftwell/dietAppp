fceaf14c5846e10fab0981979cc7cc18846c8d24
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-04-10 12:37:11 +0300

    Initial Commit

---
+++ dietApp/dietAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dietAppApp.swift
+//  dietApp
+//
+//  Created by Mehmet ali Çavuşlu on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct dietAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dietAppTests/dietAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  dietAppTests.swift
+//  dietAppTests
+//
+//  Created by Mehmet ali Çavuşlu on 10.04.2025.
+//
+
+import Testing
+@testable import dietApp
+
+struct dietAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
